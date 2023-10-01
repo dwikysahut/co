8c69a4f58b4951e0ddb39b2e7e13ac32dfa19dae
dwikysahut <dwikysahut@gmail.com>
2023-10-01 20:55:45 +0700

    first commit

---
+++ src/pages/ContactList.jsx
@@ -0,0 +1,10 @@
+/** @jsxImportSource @emotion/react */
+
+
+import {css} from '@emotion/react'
+import { containerStyle } from '../styles/styles'
+export default function ContactList(){
+    return (
+        <div css={containerStyle}  >halo</div>
+    )
+}
---
+++ src/styles/styles.js
@@ -0,0 +1,6 @@
+import { css } from "@emotion/react";
+
+const containerStyle = css({display:'flex',height:'100vh',width:'100vw'})
+
+
+export {containerStyle}
